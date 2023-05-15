3290e45a515d214c711340679103fc2497899611
Valentin Magry <valou32720@gmail.com>
2023-05-15 13:43:16 +0200

    [FIX] Correct grammatical mistakes from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -34,35 +34,28 @@ export default async function IndexPage() {
           />
         </div>
       </div>
-      <div className="flex flex-row flex-1 flex-wrap w-4/5 m-auto">
+      <div className="flex flex-row flex-1 flex-wrap w-3/5 m-auto">
         <Card>
-          <h2 className="text-4xl font-bold mb-2 text-center">About us</h2>
-          <p>
+          <h2 className="text-4xl font-bold mb-4 text-center">About us</h2>
+          <p className="text-justify">
             We are a dynamic and passionate community of gamers, and we are
             thrilled to welcome you to our website. Our community is open to all
             French-speaking players, regardless of their skill levels or game
             preferences. We cover a variety of games, from first-person shooters
-            to real-time strategy games, survival games but mostly milsim games.
+            to real-time strategy games, survival games, and mostly milsim
+            games.
             <br />
             <br />
-            Boniche gang is a name that take in the whole community, but we also
-            created some variations for different context like Task Force Boniche
-            for milsim games, Boniches Racing for F1 games, Boniche Guild for 
-            medieval games and Boniche Corp. for business shits. You are free to
-            join all of them or just one, it's up to you.
+            If we had to describe ourselves in a few words, we would say
+            that we are a community of friends who like to play together, have
+            fun, and mostly divide our IQ by 2 (or more) when we play together.
             <br />
             <br />
-            If we had to describe ourselves in a few words, we would say that we
-            are a community of friends who like to play together and have fun and mostly
-            divide our IQ by 2 (or more) when we play together.
-            <br />
-            <br />
-            We are proud to be a community that is open to everyone, regardless of
-            their religion or mental illness. For example we have a Ferrari fan in
-            our community, and we don't judge him for that.
-            So if you are a weirdos and you want to join us, don't hesitate to
-            join our Discord server. You will look like a normal person compared
-            to us.
+            We are proud to be a community that is open to everyone, regardless
+            of their religion or mental health. For example, we have a Ferrari
+            fan in our community, and we don't judge him for that. So, if you
+            are a weirdo and you want to join us, don't hesitate to join our
+            Discord server. You will look like a normal person compared to us.
           </p>
         </Card>
       </div>
