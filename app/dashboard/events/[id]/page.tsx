d5b2c8ac55e159a1dafd59fca7fcd33256219cd3
Valentin Magry <valou32720@gmail.com>
2023-05-25 10:37:49 +0200

    [FEAT] Add admin event page

--- app/dashboard/events/[id]/page.tsx
+++ app/dashboard/events/[id]/page.tsx
@@ -1,18 +1,73 @@
 import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { headers, cookies } from 'next/headers';
 
-import ComingSoon from '@/core/components/ComingSoon'
 import { Database } from '@/types/database.types';
+import Button from '@/core/components/Button';
 
-export default function EventPage() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function EventPage({ params }: Props) {
   const supabase = createServerComponentSupabaseClient<Database>({
     headers,
     cookies
   });
 
+  const { data: event, error } = await supabase
+    .from('events')
+    .select('*')
+    .eq('id', params.id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  if (!event) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-3xl font-medium tracking-tight">
+          Event N°{params.id}
+        </h1>
+        <hr className="my-8" />
+        <p className="text-xl">Error: Event not found</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      
+    <div className="flex flex-col h-full">
+      <div className="flex flex-row justify-between w-full content-center">
+        <h1 className="text-3xl font-medium tracking-tight">
+          Event N°{params.id}
+        </h1>
+
+        <div className="flex flex-row">
+          <Button style='secondary' disabled>Delete</Button>
+        </div>
+      </div>
+      <hr className="my-8" />
+      <h2 className="text-xl mb-2 font-medium tracking-tight">{event.name}</h2>
+
+      <div className="w-full flex flew-row gap-2">
+
+        <div className='w-2/3'>
+          <p>{event.description}</p>
+        </div>
+
+        <img src={event.thumbnail!} alt={event?.name} className='w-1/3 rounded-md shadow-md' />
+      </div>
+
+      <hr className="my-12" />
+
+      <div className="flex flex-row justify-between">
+        <p>Location: {event.location ?? '-'}</p>
+        <p>Start date: {new Date(event?.start_date as string).toUTCString()}</p>
+        <p>End date: {new Date(event?.end_date as string).toUTCString()}</p>
+      </div>
     </div>
-  )
+  );
 }
